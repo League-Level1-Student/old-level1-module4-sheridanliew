caf49818432903ba8a6e87735b747c8370252694
arkangel6 <league@worthymini-5.attlocal.net>
2018-11-06 18:31:46 -0800

    Jukebox (IP)- make last song stop when new song plays

--- src/Jukebox.java
+++ src/Jukebox.java
@@ -4,6 +4,8 @@
  */
 
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -11,7 +13,10 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import javazoom.jl.player.advanced.AdvancedPlayer;
@@ -19,8 +24,9 @@ import javazoom.jl.player.advanced.AdvancedPlayer;
 /* 1. Download the JavaZoom jar from here: http://bit.ly/javazoom
  * 2. Right click your project and add it as an External JAR (Under Java Build Path > Libraries).*/
 
-public class Jukebox implements Runnable {
-
+public class Jukebox implements Runnable, MouseListener {
+	JLabel faded;
+	JLabel cannnotStopTheFeeling;
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Jukebox());
 	}
@@ -29,9 +35,9 @@ public class Jukebox implements Runnable {
 
 		// 3. Find an mp3 on your computer or on the Internet.
 		// 4. Create a Song
-
+        	   //Song song=new Song("Alan Walker - Faded.mp3");
 		// 5. Play the Song
-
+           //song.play();
 		/*
 		 * 6. Create a user interface for your Jukebox so that the user can to
 		 * choose which song to play. You can use can use a different button for
@@ -39,12 +45,66 @@ public class Jukebox implements Runnable {
 		 * cover is clicked, stop the currently playing song, and play the one
 		 * that was selected.
 		 */
+           
+           JFrame frame= new JFrame();
+           frame.setVisible(true);
+           JPanel panel= new JPanel();
+           frame.add(panel);
+           faded=loadImage("Alan_Walker_-_Faded.png");
+           faded.addMouseListener(this);
+           panel.add(faded);
+           frame.pack();
+           cannnotStopTheFeeling=loadImage("Justin_Timberlake_-_Can't_Stop_the_Feeling.png");
+           cannnotStopTheFeeling.addMouseListener(this);
+           panel.add(cannnotStopTheFeeling);
+           frame.pack();
+       
           }
+          
+           
+        
 	/* Use this method to add album covers to your Panel. */
 	private JLabel loadImage(String fileName) {
 		URL imageURL = getClass().getResource(fileName);
 		Icon icon = new ImageIcon(imageURL);
 		return new JLabel(icon);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource()==faded) {
+		    Song song=new Song("Alan Walker - Faded.mp3");
+			song.play();
+		}
+		if (e.getSource()==cannnotStopTheFeeling) {
+		    Song song2=new Song("Justin Timberlake - Can't Stop the Feeling ( Lyrics Video ).mp3");
+			song2.play();
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
